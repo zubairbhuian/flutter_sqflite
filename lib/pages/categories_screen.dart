d679243ed94685169b4dfaff492b53e577a4294e
“[Zubair <[zubairbhuian@gmail.com]>
2022-04-03 08:59:45 +0600

    2.6

--- lib/pages/categories_screen.dart
+++ lib/pages/categories_screen.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sqflite/widgets/drower_navigator.dart';
+import 'package:flutter_sqflite/pages/home_screen.dart';
 
 class CategoriasScreen extends StatelessWidget {
-  const CategoriasScreen({ Key? key }) : super(key: key);
+  const CategoriasScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const DrawerNavigator(),
       appBar: AppBar(
-        title: const Text('Categories Page'),
-
+        leading: ElevatedButton(
+            onPressed: () => Navigator.push(
+                context, MaterialPageRoute(builder: (_) => const HomeScreen())),
+            child: const Icon(Icons.arrow_back)),
+        title: const Text('Categories'),
+      ),
+      body: const Center(child: Text('demo')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
