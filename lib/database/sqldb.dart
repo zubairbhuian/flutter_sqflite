47d9eeeb1a6edd82136dbdf566796fe05a090bf4
“[Zubair <[zubairbhuian@gmail.com]>
2022-04-06 10:21:43 +0600

    6.2

--- lib/database/sqldb.dart
+++ lib/database/sqldb.dart
@@ -2,6 +2,17 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class SqlDb {
+  static Database? _db;
+
+  Future<Database?> get db async {
+    if (_db == null) {
+      _db = await intialDb();
+      return _db;
+    }else{
+      return _db;
+    }
+  }
+
   intialDb() async {
     // get a location using getdatabsepath
     String databasepath = await getDatabasesPath();
@@ -10,7 +21,13 @@ class SqlDb {
     return mydb;
   }
 
-  _onCreate(Database db, int version)async {
-    await db.execute(sql)
+  _onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE "notes"(
+      id INTEGER  AUTOINCREMENT NOT NULL PRIMARY KEY,
+      notes TEXT NOT NULL
+    )
+    ''');
+    print("Create");
   }
 }
