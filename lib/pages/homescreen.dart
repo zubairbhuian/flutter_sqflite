ee386eae4141671dee1746b7489d067d1873acbb
“[Zubair <[zubairbhuian@gmail.com]>
2022-04-08 12:26:29 +0600

    8.5

--- lib/pages/homescreen.dart
+++ lib/pages/homescreen.dart
@@ -10,62 +10,65 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   SqlDb sqlDb = SqlDb();
-  Future<List<Map>> readData() async {
+  bool isloading = true;
+  List notes = [];
+  Future readData() async {
     List<Map> responce = await sqlDb.readData("SELECT * FROM notes");
-    return responce;
+    notes.addAll(responce);
+    isloading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    readData();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Page')),
-      body: ListView(
-        children: [
-          // ElevatedButton(
-          //     onPressed: () async {
-          //       await sqlDb.deleteDatebase();
-          //     },
-          //     child: const Text('Delete Database')),
-          FutureBuilder(
-              future: readData(),
-              builder:
-                  ((BuildContext context, AsyncSnapshot<List<Map>> snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemBuilder: ((context, index) {
-                        return Card(
-                          child: ListTile(
-                            title: Text("${snapshot.data![index]['note']}"),
-                            subtitle: Text("${snapshot.data![index]['title']}"),
-                            trailing: IconButton(
-                              onPressed: () async {
-                                int response = await sqlDb.deleteData(
-                                    "DELETE FROM notes WHERE id = ${snapshot.data![index]['id']}");
-                                if (response > 0) {
-                                  Navigator.of(context).pushAndRemoveUntil(
-                                      MaterialPageRoute(
-                                          builder: (_) => const HomeScreen()),
-                                      (route) => false);
-                                }
-                              },
-                              icon: const Icon(
-                                Icons.delete,
-                                color: Colors.red,
-                              ),
+      body: isloading == true
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView(
+              children: [
+                TextButton(
+                    onPressed: () async {
+                      await sqlDb.deleteDatebase();
+                    },
+                    child: const Text('Delete Database')),
+                ListView.builder(
+                    itemCount: notes.length,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: ((context, index) {
+                      return Card(
+                        child: ListTile(
+                          title: Text("${notes[index]['note']}"),
+                          subtitle: Text("${notes[index]['title']}"),
+                          trailing: IconButton(
+                            onPressed: () async {
+                              int response = await sqlDb.deleteData(
+                                  "DELETE FROM notes WHERE id = ${notes[index]['id']}");
+                              if (response > 0) {
+                                notes.removeWhere((element) =>
+                                    element['id'] == notes[index]['id']);
+                                setState(() {});
+                              }
+                            },
+                            icon: const Icon(
+                              Icons.delete,
+                              color: Colors.red,
                             ),
                           ),
-                        );
-                      }));
-                }
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }))
-        ],
-      ),
+                        ),
+                      );
+                    }))
+              ],
+            ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).pushNamed("adddata");
